259b19674f0ef1fbbba5dd3a5240904fe43a1638
wovri <adammisiak2@gmail.com>
2022-12-11 23:08:42 +0100

    [Bugfix] Error without internet connection - external ip adress is only checked on button click - when ip is establish, clicking on button will result in coping ip into clipboard

--- GUI/Menu.gd
+++ GUI/Menu.gd
@@ -16,15 +16,26 @@ func _on_MultiplayerButton_pressed():
 	main_node.game_mode(1, ADRESS[$"%OptionButton".get_selected()])
 	queue_free()
 
-
 func _ready():
 	$HTTPRequest.connect("request_completed", self, "_on_request_completed")
-	var err = $HTTPRequest.request("https://api.ipify.org/?format=json")
-	if err != 0:
-		var err_comm ="Ipify non connected: " + str(err)
-		$"%PublicIPButton".set_text(err_comm)
 
-func _on_request_completed(result, response_code, headers, body):
+func _on_request_completed(_result, _response_code, _headers, body):
+	if _result != 0:
+		return
 	var json = JSON.parse(body.get_string_from_utf8())
-#	$"%PublicIPButton".set_text(str(json.result["ip"]))
-	$HTTPRequest.queue_free()
+	if json.result.has("ip"):
+		$"%PublicIPButton".set_text(str(json.result["ip"]))
+		$HTTPRequest.queue_free()
+
+
+func _on_PublicIPButton_pressed():
+	#warning-ignore:return_value_discarded
+	if has_node("HTTPRequest"):
+		$"%PublicIPButton".set_text("...")
+		var err = $HTTPRequest.request("https://api.ipify.org/?format=json")
+		if err != 0:
+			var err_comm ="Ipify non connected: " + str(err)
+			$"%PublicIPButton".set_text(err_comm)
+			#$HTTPRequest.queue_free()
+	else:
+		OS.set_clipboard($Background/PublicIPButton.text)
