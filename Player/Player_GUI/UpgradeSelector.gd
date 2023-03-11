31baf9eb6f481c41da20fce3cbc9796e4b5ebe9d
wovri <adammisiak2@gmail.com>
2023-03-11 14:13:41 +0100

    Set up SpecialUpgrade display in Upgrade

--- Player/Player_GUI/UpgradeSelector.gd
+++ Player/Player_GUI/UpgradeSelector.gd
@@ -7,10 +7,12 @@ onready var upgrade_container = $"%UpgradeContainer"
 onready var points_left = $"%PointsLeft"
 onready var label_self_destroyed = $"%LabelSelfDestroyed"
 onready var background = $"%Background"
+onready var title = $"%Title"
+onready var label_points = $"%LabelPoints"
 
 func _ready():
 	Transfer.connect("recive_player_possible_upgrades", self, "show_upgrades")
-	#show_upgrades({"State": "SelfDestroyed", "Points": 2, "Upgrades": [["Tank", "Speed"]]})
+	#show_upgrades({"State": "Winner", "Points": 2, "Upgrades": {["Tank", "BaseAmmoType"]: 2}})
 
 
 func show_upgrades(data):
@@ -22,12 +24,20 @@ func show_upgrades(data):
 		if state == "SelfDestroyed":
 			disable_buttons(true)
 		return
-	points_left.text = str(data.Points)
 	state = data.State
+	points_left.text = str(data.Points)
+	if data.State == "Winner":
+		title.set_text("Choose a special upgrade for")
+		label_points.set_text("Rounds: ")
+		background.get_stylebox("panel").set_bg_color(Color(0.5,0.2,0.8,0.3))
 	for upgrade_info in data.Upgrades:
+		print(data.Upgrades)
 		var inst = upgrade_tscn.instance()
 		inst.connect("upgrade_selected", self, "_on_upgrade_selected")
-		inst.init(upgrade_info)
+		var data1 = upgrade_info.duplicate()
+		if typeof(data.Upgrades) == TYPE_DICTIONARY:
+			data1.append(data.Upgrades[upgrade_info])
+		inst.init(data1)
 		upgrade_container.add_child(inst)
 	match data.State:
 		"Normal":
@@ -38,12 +48,20 @@ func show_upgrades(data):
 			label_self_destroyed.show()
 			disable_buttons(true)
 		"Winner":
+			title.set_text("Choose a special upgrade for")
+			label_points.set_text("Rounds: ")
 			background.get_stylebox("panel").set_bg_color(Color(0.5,0.2,0.8,0.3))
 	if data.Points == 0:
 		disable_buttons(true)
 	show()
 
 func _on_upgrade_selected(upgrade):
+	if state == "Winner":
+		disable_buttons(true)
+		var value = upgrade.pop_back()
+		var dict = {upgrade: value}
+		Transfer.fetch_player_possible_upgrades(dict)
+		return
 	points_left.text = str(int(points_left.text) - 1)
 	if !choosen_upgrades.has(upgrade):
 		choosen_upgrades[upgrade] = 1
