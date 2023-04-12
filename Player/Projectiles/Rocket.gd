4007bd27111a7102f5f1c8f43333312e9eec3230
wovri <adammisiak2@gmail.com>
2023-04-12 22:51:54 +0200

    Rocket radius depends on target distance :dizzy:

--- Player/Projectiles/Rocket.gd
+++ Player/Projectiles/Rocket.gd
@@ -35,6 +35,10 @@ func _integrate_forces(state):
 
 func _physics_process(delta):
 	if is_instance_valid(target):
+		var distance =  min(global_position.distance_to(target.global_position), 500)
+		$Sprite.get_material().set_shader_param("radius", distance)
+		print((-0.03 * distance) + 20.0)
+		#$Sprite.get_material().set_shader_param("speed", (-0.03 * distance) + 20.0)
 		var rot = navigation_agent.get_next_location().angle_to_point(position)
 		rotation = lerp_angle(rotation, rot, delta * 8)
 	else:
